4311e7708424e371dd33b9cecb1ce7dc64a670de
manybugsdev <manybugsdev@users.noreply.github.com>
2024-06-08 22:04:27 +0900

    direct import

--- cli.ts
+++ cli.ts
@@ -1,5 +1,5 @@
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
-import { join } from "@std/path";
+import { join } from "jsr:@std/path@^0.225.1";
 
 await new Command()
   .command(
