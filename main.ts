1111ccc41af7a05eb34b2ffb2ae9432dfe948ba2
manybugsdev <mbd@manybugs.dev>
2024-10-25 17:27:29 +0900

    $ is good

--- main.ts
+++ main.ts
@@ -1,35 +1,106 @@
-import { serveDir } from "@std/http/file-server";
+import { serveDir } from "jsr:@std/http/file-server";
 
-function post({ path }: Record<string, string | undefined>): Response {
-    return serveDir();
-}
+type Handler = (req: Request) => Response | Promise<Response>;
 
-function home(): Response {
-    return new Response("home");
-}
-
-function router(
+type RouterHandler = (
+    groups: Record<string, string | undefined>,
     req: Request,
-    route: Record<
-        string,
-        (groups: Record<string, string | undefined>) => Response
-    >,
-): Response | null {
-    for (const pathname in route) {
-        const pattern = new URLPattern({ pathname });
-        const match = pattern.exec(req.url);
-        if (match) {
-            return route[pathname](match.pathname.groups);
+) => Response | Promise<Response>;
+
+type Post = {
+    md: string;
+    html: string;
+    headers: Record<string, string>;
+};
+
+const $ = (
+    tag: string,
+    attrs?: Record<string, string>,
+    ...children: string[]
+) => `<${tag} ${
+    attrs
+        ? Object.keys(attrs).map((key) => `${key}="${attrs[key]}"`).join(" ")
+        : ""
+}${children.length ? `>${children.join("")}</${tag}>` : "/>"}`;
+
+const $layout = ({ title, body }: { title: string; body: string }) =>
+    $(
+        "html",
+        { lang: "ja" },
+        $(
+            "head",
+            {},
+            $("meta", { charset: "utf8" }),
+            $("meta", {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1.0",
+            }),
+            $("title", {}, title),
+            $("link", { rel: "stylesheet", href: "style.css" }),
+        ),
+        $("body", {}, body),
+    );
+
+const notFound = () => new Response("404 not found.", { status: 404 });
+
+const pipe = (...handlers: Handler[]) => async (req: Request) => {
+    for (const handler of handlers) {
+        const res = await handler(req);
+        if (res.status !== 404) return res;
+    }
+    return notFound();
+};
+
+const router =
+    (route: Record<string, RouterHandler>) => async (req: Request) => {
+        for (const pathname in route) {
+            const handler = route[pathname];
+            const pattern = new URLPattern({ pathname });
+            const match = pattern.exec(req.url);
+            if (match) {
+                return await handler(match.pathname.groups, req);
+            }
+        }
+        return notFound();
+    };
+
+const html = (body: string) =>
+    new Response(body, {
+        status: 200,
+        headers: { "content-type": "text/html;charset=utf8" },
+    });
+
+const collectPosts = async (path: string) => {
+    let posts: Record<string, Post> = {};
+    for await (const ent of Deno.readDir(path)) {
+        if (ent.isDirectory) {
+            posts = {
+                ...posts,
+                ...(await collectPosts(`${path}/${ent.name}`)),
+            };
+        }
+        if (ent.isFile && ent.name.endsWith(".md")) {
+            const basename = ent.name.split(".").slice(0, -1).join(".");
+            posts[`${path}/${basename}/`] = { md: "", html: "", headers: {} };
         }
     }
-    return null;
-}
+    return posts;
+};
 
 if (import.meta.main) {
-    Deno.serve((req) =>
-        router(req, {
-            "/": home,
-            "/posts/:path*": post ?? serveDir(req, { fsRoot: "." }),
-        }) ?? serveDir(req, { fsRoot: "./static" })
+    const posts = await collectPosts("posts");
+    Deno.serve(
+        pipe(
+            router({
+                "/": () =>
+                    html(
+                        $layout({
+                            title: "Home | manybugs.dev",
+                            body: "<h1>hi</h1>",
+                        }),
+                    ),
+            }),
+            (req) => serveDir(req, { fsRoot: "./static" }),
+        ),
     );
 }
